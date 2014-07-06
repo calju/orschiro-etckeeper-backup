050fdbdd17437f1081d438f6def1ce76d148908a
Robert Orzanna <orschiro@gmail.com>
2014-07-07 00:27:19 +0200

    saving uncommitted changes in /etc prior to apt run

--- profile.d/jre.csh
+++ profile.d/jre.csh
@@ -1,3 +1,4 @@
-setenv J2REDIR /opt/java/jre
-setenv PATH ${PATH}:/opt/java/jre/bin
-setenv JAVA_HOME /opt/java/
+setenv JAVA_HOME "/usr/lib/jvm/java-7-openjdk/jre"
+
+# Enable this for non-reparenting window managers
+#setenv _JAVA_AWT_WM_NONREPARENTING 1
--- profile.d/jre.sh
+++ profile.d/jre.sh
@@ -1,3 +1,4 @@
-export J2REDIR=/opt/java/jre
-export PATH=$PATH:/opt/java/jre/bin
-export JAVA_HOME=${JAVA_HOME:-/opt/java/jre}
+export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-7-openjdk/jre}
+
+# Enable this for non-reparenting window managers
+#export _JAVA_AWT_WM_NONREPARENTING=1
